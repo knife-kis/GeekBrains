89c1129f30e82569a26d7ea86e4faba3c1539185
knife-kis <36123852+knife-kis@users.noreply.github.com>
2019-12-13 18:15:51 +0700

    Домашнее задание v.1.2

--- lesson02/src/com/lesson02/tarnovskiy/App.java
+++ lesson02/src/com/lesson02/tarnovskiy/App.java
@@ -1,6 +1,7 @@
 package com.lesson02.tarnovskiy;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @author Maksim Tarnovskiy
@@ -8,25 +9,20 @@ import java.util.Arrays;
 public class App {
 
     public static void main(String[] args) throws MyArraySizeExeption {
-        int[][] array = new int[4][4];
-        int[][] array2 = new int[2][2];
-        int[][] array3 = new int[4][3];
-        addArray(array);
-        addArray(array2);
-        addArray(array3);
-    }
-    static void addArray(int[][] array){
-        try {
-            if (array.length == 4 && array[0].length == 4) {
-                for (int i = 0; i < array.length; i++) {
-                    for (int j = 0; j < array.length; j++) {
-                        array[i][j] = array.length;
-                    }
-                }
-            } else throw new MyArraySizeExeption();
-            System.out.println(Arrays.deepToString(array));
-        } catch (MyArraySizeExeption e){
-            System.out.println("Вы ввели неверную размерность массива, вы ввели: [" + array.length + "][" + array[0].length + "]" );
-        }
+        Scanner input;
+        String[][] array1;
+        int[][] arrayInt;
+        int sumArray;
+        FillArray fillArray = new FillArray();
+
+        input = new Scanner(System.in);
+        array1 = fillArray.fill(Integer.parseInt(input.next()), Integer.parseInt(input.next())); // задаем размерность массива
+        array1 = fillArray.inputArray(array1); // заполняем массив
+        arrayInt = fillArray.convertInt(array1);
+        sumArray = fillArray.sum(arrayInt);
+
+        System.out.println(Arrays.deepToString(arrayInt));
+        System.out.println("Сумма всех чисел ровна: " + sumArray);
+
     }
 }
